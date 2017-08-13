6ec156f768ab2f551b7f31c0ceb7d69086ee159a
Kevin van der Werff <kevinsebastianate@hotmail.com>
2017-08-13 19:29:26 +0200

    add scss project strutcture & input project name

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,24 +1,22 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-  method1() {
-    this.log('method 1 just ran');
-  }
-
-  _private_method2() {
-    this.log('method 2 just ran');
-  }
-
-  writing() {
-    this.fs.copyTpl(
-      this.templatePath('index.html'),
-      this.destinationPath('public/index.html'),
-      { title: 'Templating with Yeoman' }
-    );
-    this.fs.copyTpl(
-      this.templatePath('scss/main.scss'),
-      this.destinationPath('public/main/main.scss'),
-      { boxSizing: 'border-box'}
-    );
+  prompting() {
+    return this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname // Default to current folder name
+    }]).then((answers) => {
+      this.fs.copyTpl(
+        this.templatePath('index.html'),
+        this.destinationPath('public/index.html'),
+        { title: answers.name }
+      );
+      this.fs.copyTpl(
+        this.templatePath('scss'),
+        this.destinationPath('public/scss')
+      );
+    });
   }
 };
